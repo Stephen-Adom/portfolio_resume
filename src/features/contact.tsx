a90014f2d04365214e9207c556f30a06892ee58b
Stephen-Adom <stephen.alaska94@hotmail.com>
2023-08-05 19:25:56 +0000

    add react hook form to contact form

--- src/features/contact.tsx
+++ src/features/contact.tsx
@@ -3,8 +3,49 @@ import { TitleLabel } from "../components";
 import { InputText } from "primereact/inputtext";
 import { InputTextarea } from "primereact/inputtextarea";
 import { PhoneInput } from "react-international-phone";
+import { useForm } from "react-hook-form";
+
+type ContactFormType = {
+	fullname: string;
+	email: string;
+	phone: string;
+	subject: string;
+	message: string;
+};
 
 const Contact = () => {
+	const form = useForm<ContactFormType>({
+		defaultValues: {
+			fullname: "",
+			email: "",
+			phone: "",
+			subject: "",
+			message: "",
+		},
+	});
+
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = form;
+
+	const onSubmit = (data: ContactFormType) => {
+		console.log(data);
+	};
+
+	const errorBorder = (field: keyof ContactFormType) => {
+		if (errors && errors[field]) {
+			return "p-invalid";
+		}
+	};
+
+	const errorMessage = (field: keyof ContactFormType) => {
+		if (errors && errors[field]) {
+			return <span className="block text-red-600 text-sm mt-1">{errors[field]?.message}</span>;
+		}
+	};
+
 	return (
 		<div className="my-16 lg:my-40 services-section" id="contact">
 			<TitleLabel>
@@ -18,21 +59,41 @@ const Contact = () => {
 				<p className="text-2xl">stephen.alaska94@gmail.com</p>
 
 				<form
-					action=""
+					onSubmit={handleSubmit(onSubmit)}
 					className="grid grid-cols-1 gap-4 mt-12 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2"
 				>
 					<div className="form-group">
 						<label htmlFor="fullname">
 							FULL NAME <span className="text-red-600">*</span>
 						</label>
-						<InputText placeholder="Your Full Name" id="fullname" />
+						<InputText
+							placeholder="Your Full Name"
+							id="fullname"
+							className={`${errorBorder("fullname")}`}
+							{...register("fullname", {
+								required: "Enter your full name",
+							})}
+						/>
+						{errorMessage("fullname")}
 					</div>
 
 					<div className="form-group">
 						<label htmlFor="email">
 							EMAIL <span className="text-red-600">*</span>
 						</label>
-						<InputText placeholder="Your Email Address" id="email" />
+						<InputText
+							placeholder="Your Email Address"
+							id="email"
+							className={`${errorBorder("fullname")}`}
+							{...register("email", {
+								required: "Enter your email address",
+								pattern: {
+									value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+									message: "Invalid email address",
+								},
+							})}
+						/>
+						{errorMessage("email")}
 					</div>
 
 					<div className="form-group">
@@ -44,18 +105,35 @@ const Contact = () => {
 						<label htmlFor="subject">
 							SUBJECT <span className="text-red-600">*</span>
 						</label>
-						<InputText placeholder="Your Email Address" id="subject" />
+						<InputText
+							placeholder="Your message subject/title"
+							className={`${errorBorder("fullname")}`}
+							id="subject"
+							{...register("subject", {
+								required: "Enter your message subject",
+							})}
+						/>
+						{errorMessage("email")}
 					</div>
 
 					<div className="col-span-1 md:col-span-2 form-group">
 						<label htmlFor="message">
 							MESSAGE <span className="text-red-600">*</span>
 						</label>
-						<InputTextarea placeholder="Write your message here..." rows={2} cols={20} />
+						<InputTextarea
+							placeholder="Write your message here..."
+							rows={2}
+							cols={20}
+							className={`${errorBorder("fullname")}`}
+							{...register("message", {
+								required: "Enter your message",
+							})}
+						/>
+						{errorMessage("message")}
 					</div>
 
 					<button
-						type="button"
+						type="submit"
 						className="w-[90%] lg:w-[70%] py-5 text-md text-white rounded-full linear-gradient"
 					>
 						SEND MESSAGE
