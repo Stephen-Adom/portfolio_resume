ccaabd75e495e3c51872e83a3f7af2c84a3d2609
Stephen-Adom <stephen.alaska94@hotmail.com>
2023-08-05 15:51:44 +0000

    add transition effect to project cards

--- src/components/project-card.tsx
+++ src/components/project-card.tsx
@@ -10,9 +10,9 @@ const ProjectCard = ({ project, index }: propType) => {
 		<div className={`${(index === 0 || index === 5) && "col-span-1 lg:col-span-2"}`}>
 			<div
 				style={{ backgroundImage: `url(${project.image})` }}
-				className={`project-card cursor-pointer`}
+				className={`project-card cursor-pointer relative after:content-[''] after:absolute after:top-0 after:left-0 after:w-full after:h-full after:bg-gray-900/10`}
 			>
-				<div className="absolute flex items-center flex-wrap gap-2 tech bottom-4 left-4">
+				<div className="absolute flex items-center flex-wrap gap-2 tech bottom-4 left-4 z-10">
 					{project.tech_stacks.length &&
 						project.tech_stacks.map((tech) => {
 							return (
