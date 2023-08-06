59223a4ded5eda2fb04cd15c2ed5238920991d89
Stephen-Adom <stephen.alaska94@hotmail.com>
2023-08-06 17:16:50 +0000

    set loading state for contact form

--- src/features/contact.tsx
+++ src/features/contact.tsx
@@ -5,6 +5,7 @@ import { InputTextarea } from "primereact/inputtextarea";
 import { PhoneInput } from "react-international-phone";
 import { useForm } from "react-hook-form";
 import emailjs, { EmailJSResponseStatus } from "@emailjs/browser";
+import { useState } from "react";
 
 type ContactFormType = {
 	fullname: string;
@@ -15,6 +16,8 @@ type ContactFormType = {
 };
 
 const Contact = () => {
+	const [loading, setLoading] = useState(true);
+
 	const form = useForm<ContactFormType>({
 		defaultValues: {
 			fullname: "",
@@ -32,7 +35,7 @@ const Contact = () => {
 	} = form;
 
 	const onSubmit = (data: ContactFormType) => {
-		console.log(data);
+		setLoading(true);
 		const form = document.createElement("form");
 		form.style.display = "none";
 
@@ -56,9 +59,11 @@ const Contact = () => {
 		emailjs.sendForm("service_iljohou", "template_gicdnqz", form, "7JGwtKQcRgR4XdTIc").then(
 			(result: EmailJSResponseStatus) => {
 				console.log(result.text);
+				setLoading(false);
 			},
 			(error) => {
 				console.log(error.text);
+				setLoading(false);
 			}
 		);
 	};
@@ -162,10 +167,37 @@ const Contact = () => {
 					</div>
 
 					<button
+						disabled={loading}
 						type="submit"
 						className="w-[90%] lg:w-[70%] py-5 text-md text-white rounded-full linear-gradient"
 					>
-						SEND MESSAGE
+						{loading ? (
+							<div className="flex items-center justify-center">
+								<svg
+									className="animate-spin mr-2 h-5 w-5 text-white"
+									xmlns="http://www.w3.org/2000/svg"
+									fill="none"
+									viewBox="0 0 24 24"
+								>
+									<circle
+										className="opacity-25"
+										cx="12"
+										cy="12"
+										r="10"
+										stroke="currentColor"
+										strokeWidth="4"
+									></circle>
+									<path
+										className="opacity-75"
+										fill="currentColor"
+										d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+									></path>
+								</svg>
+								<span className="animate-pulse">SENDING...</span>
+							</div>
+						) : (
+							"SEND MESSAGE"
+						)}
 					</button>
 				</form>
 			</div>
