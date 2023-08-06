b963607678b139acd121d60566b2d4c53558239c
Stephen-Adom <stephen.alaska94@hotmail.com>
2023-08-06 19:23:33 +0000

    add toastr for notification

--- src/features/contact.tsx
+++ src/features/contact.tsx
@@ -1,11 +1,13 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { HiOutlineEnvelope } from "react-icons/hi2";
-import { TitleLabel } from "../components";
 import { InputText } from "primereact/inputtext";
 import { InputTextarea } from "primereact/inputtextarea";
 import PhoneInput, { formatPhoneNumberIntl, isValidPhoneNumber } from "react-phone-number-input";
 import { useForm } from "react-hook-form";
 import emailjs, { EmailJSResponseStatus } from "@emailjs/browser";
+import toast from "react-hot-toast";
+import { TitleLabel } from "../components";
+import { BsCheckAll } from "react-icons/bs";
 
 type ContactFormType = {
 	fullname: string;
@@ -79,7 +81,7 @@ const Contact = () => {
 				reset();
 				setPhonenumber("");
 				setPhoneError(false);
-				console.log(result.text);
+				notify();
 				setLoading(false);
 			},
 			(error) => {
@@ -106,6 +108,44 @@ const Contact = () => {
 			return <span className="block text-red-600 text-sm mt-1">{errors[field]?.message}</span>;
 		}
 	};
+
+	const notify = () =>
+		toast.custom(
+			(t) => (
+				<div
+					className={`${
+						t.visible ? "animate-enter" : "animate-leave"
+					} max-w-md w-full custom-toast shadow-lg rounded-lg pointer-events-auto flex ring-1`}
+				>
+					<div className="flex-1 w-0 p-4">
+						<div className="flex items-start">
+							<div className="flex-shrink-0 pt-0.5">
+								<div className="h-10 w-10 rounded-full flex items-center justify-center toast-icon-container">
+									<BsCheckAll className="text-lg" />
+								</div>
+							</div>
+							<div className="ml-3 flex-1">
+								<p className="text-sm font-medium">Your message has been sent!</p>
+								<p className="mt-1 text-sm text-gray-500">
+									I will get in touch as soon as possible
+								</p>
+							</div>
+						</div>
+					</div>
+					<div className="flex border-l border-gray-200">
+						<button
+							onClick={() => toast.dismiss(t.id)}
+							className="w-full border border-transparent rounded-none rounded-r-lg p-4 flex items-center justify-center text-sm font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+						>
+							Close
+						</button>
+					</div>
+				</div>
+			),
+			{
+				duration: 4000,
+			}
+		);
 
 	return (
 		<div className="my-16 lg:my-40 services-section" id="contact">
