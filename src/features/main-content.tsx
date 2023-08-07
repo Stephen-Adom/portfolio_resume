729f327333e5a4f2f846ad64d19a935dfbe26667
Stephen-Adom <stephen.alaska94@hotmail.com>
2023-08-07 09:13:05 +0000

    fix extra space on mobile screen

--- src/features/main-content.tsx
+++ src/features/main-content.tsx
@@ -9,7 +9,7 @@ import ScrollSpy from "react-ui-scrollspy";
 
 const MainContent = () => {
 	return (
-		<div className="pt-7 main-content ml-0 sm:ml-0 md:ml-0 lg:ml-[30%] w-ful lg:w-[60%] p-3 lg:p-0">
+		<div className="pt-7 main-content ml-0 sm:ml-0 md:ml-0 lg:ml-[30%] w-full lg:w-[60%] p-3 lg:p-0">
 			<ScrollSpy>
 				<Introduction />
 				<About />
